6e52a34fe518a3f7aea87afda8c0ed10ed776ccd
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-12-09 16:08:28 +0000

    update using angular material & re-designing the selecting the registration type

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,11 @@
 
 var greeting = 'hello';
 console.log(greeting + ' Guv');
+
+var questions = {
+  question1: 'What is your plate',
+  question2: 'abc',
+  question3: 'abcdef'
+}
+
+console.log(questions);
